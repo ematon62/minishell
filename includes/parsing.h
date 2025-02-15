00e6452603abcef21193838612976ca92b722750
Emmanuel Maton <ematon@c1r6p17.42lehavre.fr>
2025-02-15 18:00:33 +0100

    fais chier

--- includes/parsing.h
+++ includes/parsing.h
@@ -6,7 +6,7 @@
 /*   By: ematon <ematon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/09 19:40:24 by ematon            #+#    #+#             */
-/*   Updated: 2025/02/15 13:44:03 by ematon           ###   ########.fr       */
+/*   Updated: 2025/02/15 14:07:05 by ematon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,16 @@ typedef enum e_token
 	WORD,
 	IO_IN,
 	IO_OUT,
-	IO_HD,
+	IO_HEREDOC,
 	IO_APPEND,
 	IO_PIPE
 }	t_token;
+
+typedef struct s_tokens
+{
+	
+};
+
 
 //Shell
 t_shell	*init_shell(char **envp);
