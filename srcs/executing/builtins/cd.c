d40aaa4e10e4b369e7cb47c32aacc2016e5d4bc4
Emmanuel Maton <ematon@c1r6p12.42lehavre.fr>
2025-03-07 13:39:48 +0100

    **cd fix

--- srcs/executing/builtins/cd.c
+++ srcs/executing/builtins/cd.c
@@ -6,31 +6,44 @@
 /*   By: ematon <ematon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/18 15:32:52 by adcisse           #+#    #+#             */
-/*   Updated: 2025/03/06 17:47:36 by ematon           ###   ########.fr       */
+/*   Updated: 2025/03/07 12:14:00 by ematon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "executing.h"
 
+static bool	check_path(char *path)
+{
+	struct stat	path_stat;
+
+	stat(path, &path_stat);
+	if (access(path, F_OK))
+		return (ft_putstr_fd("cd: No such file or directory\n", 2), 1);
+	if (!S_ISDIR(path_stat.st_mode))
+		return (ft_putstr_fd("cd: Not a directory\n", STDERR_FILENO), 1);
+	if (access(path, X_OK))
+		return (ft_putstr_fd("cd: ", 2), ft_putstr_fd(path, 2),
+			ft_putstr_fd(": Permission denied\n", 2), 1);
+	return (0);
+}
+
 int	builtin_cd(char **args, t_shell *sh)
 {
-	char	*path;
-	char	cwd[PATH_MAX];
-	char	*oldpwd;
+	char		cwd[PATH_MAX];
 
-	oldpwd = get_env_value(sh->env, "PWD");
-	if (!oldpwd)
-		return (1);
 	if (!args[1])
-		return (ft_putstr_fd("cd: no argument\n", STDERR_FILENO),
-			free(oldpwd), 1);
-	path = args[1];
-	if (chdir(path) != 0)
-		return (ft_putstr_fd("cd: invalid path\n", STDERR_FILENO),
-		free(oldpwd), 1);
-	getcwd(cwd, PATH_MAX);
-	update_env_var(&sh->env, "OLDPWD", oldpwd);
-	free(oldpwd);
+		return (ft_putstr_fd("cd: invalid arguments\n", STDERR_FILENO), 1);
+	else if (args[2])
+		return (ft_putstr_fd("cd: too many arguments\n", STDERR_FILENO), 1);
+	if (check_path(args[1]))
+		return (1);
+	if (!getcwd(cwd, PATH_MAX))
+		return (perror("getcwd"), 1);
+	update_env_var(&sh->env, "OLDPWD", cwd);
+	if (chdir(args[1]) != 0)
+		return (perror("chdir"), 1);
+	if (!getcwd(cwd, PATH_MAX))
+		return (perror("getcwd"), 1);
 	update_env_var(&sh->env, "PWD", cwd);
 	return (0);
 }
