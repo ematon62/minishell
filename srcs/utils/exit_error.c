ae32524ea57b9e58c0b026ce901c5e875782dd52
Emmanuel Maton <ematon@c1r6p12.42lehavre.fr>
2025-03-12 13:53:30 +0100

    **Fix check_invalid_path imprime pas sur stderr

--- srcs/utils/exit_error.c
+++ srcs/utils/exit_error.c
@@ -6,7 +6,7 @@
 /*   By: ematon <ematon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/14 13:36:22 by ematon            #+#    #+#             */
-/*   Updated: 2025/03/11 16:18:09 by ematon           ###   ########.fr       */
+/*   Updated: 2025/03/12 13:51:07 by ematon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,16 +23,20 @@ int	check_invalid_path(char *path, t_cmd *cmd, t_shell *sh)
 	if (!path)
 	{
 		if (ft_strchr(cmd->args[0], '/'))
-			return (printf("%s: No such file or directory\n", cmd->args[0]),
+			return (ft_putstr_fd(cmd->args[0], STDERR_FILENO),
+				ft_putstr_fd(": No such file or directory\n", STDERR_FILENO),
 				free_sh_cmds(sh), 127);
-		return (printf("%s: command not found\n", cmd->args[0]),
+		return (ft_putstr_fd(cmd->args[0], STDERR_FILENO),
+			ft_putstr_fd(": command not found\n", STDERR_FILENO),
 			free_sh_cmds(sh), 127);
 	}
 	if (is_directory(path))
-		return (printf("%s: Is a directory\n", cmd->args[0]),
+		return (ft_putstr_fd(cmd->args[0], STDERR_FILENO),
+			ft_putstr_fd(": Is a directory\n", STDERR_FILENO),
 			free_sh_cmds(sh), 126);
 	if (access(path, X_OK))
-		return (printf("%s: Permission denied\n", cmd->args[0]),
+		return (ft_putstr_fd(cmd->args[0], STDERR_FILENO),
+			ft_putstr_fd(": Permission denied\n", STDERR_FILENO),
 			free_sh_cmds(sh), 126);
 	return (0);
 }
